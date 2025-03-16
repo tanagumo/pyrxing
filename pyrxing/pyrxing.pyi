5cf308cf72cbd83d665e3cb527222a6f5edf3d8d
tanagumo <nagumo5683@gmail.com>
2025-03-16 23:22:28 +0900

    accept barcode formats and improve performance

--- pyrxing/pyrxing.pyi
+++ pyrxing/pyrxing.pyi
@@ -1,4 +1,57 @@
+import sys
+from typing import Literal
+
 import PIL.Image
+
+
+if sys.version_info >= (3, 12):
+    type BarcodeFormat = Literal[
+        'AZTEC',
+        'CODABAR',
+        'CODE_39',
+        'CODE_93',
+        'CODE_128',
+        'DATA_MATRIX',
+        'EAN_8',
+        'EAN_13',
+        'ITF',
+        'MAXICODE',
+        'PDF_417',
+        'QR_CODE',
+        'MICRO_QR_CODE',
+        'RECTANGULAR_MICRO_QR_CODE',
+        'RSS_14',
+        'RSS_EXPANDED',
+        'TELEPEN',
+        'UPC_A',
+        'UPC_E',
+        'UPC_EAN_EXTENSION',
+        'DX_FILM_EDGE',
+    ]
+else:
+    BarcodeFormat = Literal[
+        'AZTEC',
+        'CODABAR',
+        'CODE_39',
+        'CODE_93',
+        'CODE_128',
+        'DATA_MATRIX',
+        'EAN_8',
+        'EAN_13',
+        'ITF',
+        'MAXICODE',
+        'PDF_417',
+        'QR_CODE',
+        'MICRO_QR_CODE',
+        'RECTANGULAR_MICRO_QR_CODE',
+        'RSS_14',
+        'RSS_EXPANDED',
+        'TELEPEN',
+        'UPC_A',
+        'UPC_E',
+        'UPC_EAN_EXTENSION',
+        'DX_FILM_EDGE',
+    ]
 
 
 class DecodingError(Exception): ...
@@ -23,5 +76,5 @@ class DecodeResult:
     def format(self) -> str: ...
 
 
-def decode(value: str | PIL.Image.Image) -> list[DecodeResult]: ...
-
+def read_barcode(image: str | PIL.Image.Image, *, formats: list[BarcodeFormat] | None = None) -> DecodeResult | None: ...
+def read_barcodes(image: str | PIL.Image.Image, *, formats: list[BarcodeFormat] | None = None) -> list[DecodeResult]: ...
